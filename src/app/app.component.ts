be2e4051430b7699e8e6e48a2d9aa86045f08521
Sean Perkins <sean@ionic.io>
2022-04-21 12:29:37 -0400

    chore(): dev build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  min = '2022-03-01';
+  max = '2022-04-07';
+  initialDate = '2022-04-05T10:00:00';
 }
